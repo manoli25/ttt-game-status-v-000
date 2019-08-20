b51868b981d7dd20e98a7b74cc23a7fef633532b
Manny Georganas <manoli.georganas@gmail.com>
2019-08-20 22:59:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,10 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8], # Bottom row
+  [0,4,8], # Diagonal from top left to bottom right
+  [2,4,6] # Diagonal from top right to bottom left
+]
