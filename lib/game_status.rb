bb84a47b4b5b2e4428737ac23c8f2b382fbf556a
Manny Georganas <manoli.georganas@gmail.com>
2019-08-21 00:09:07 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,10 +24,18 @@ WIN_COMBINATIONS = [
 #     end
 #   end
 # end
+
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] &&
     board[combo[1]] == board[combo[2]] &&
     position_taken?(board, combo[0])
   end
+end
+
+def full?(board)
+  board.all? { |move| position_taken?(board,move)  }
+
+
+
 end
